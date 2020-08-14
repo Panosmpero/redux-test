e485a7a5f4a18f67ec7e6f299d067a86584c6d29
Beros <59673191+Panosmpero@users.noreply.github.com>
2020-08-14 21:14:27 +0100

    App Home About

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./components/App";
 
-const Hi = () => {
-  // debugger; // show on sources the original code
-  return <div>Hello React</div>;
-};
-
-render(<Hi />, document.getElementById("app"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
